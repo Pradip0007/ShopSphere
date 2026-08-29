7411d8efaf19d491ce5e177af0593448030cd5d4
pradiphatui <pradiphatui3290@outlook.com>
2026-08-29 21:55:46 +0530

    chore: adopt Biome Husky and commitlint

---
+++ commitlint.config.js
@@ -0,0 +1,13 @@
+/** @type {import('@commitlint/types').UserConfig} */
+export default {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      ['feat', 'fix', 'chore', 'docs', 'style', 'refactor', 'perf', 'test', 'build', 'ci'],
+    ],
+    'subject-case': [2, 'never', ['upper-case']],
+    'header-max-length': [2, 'always', 100],
+  },
+};
